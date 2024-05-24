f1194a84a2ffc40fe633b9c00a2ff87855c484a4
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2024-05-24 13:48:52 -0400

    sun arc and moon attempt

---
+++ components/TechSkills.jsx
@@ -0,0 +1,11 @@
+
+const TechSkills = () => {
+  return (
+    <div>
+      <h2>Technical Skills</h2>
+      <h3>Python, GDScript, Godot Engine, Git, GitHub, JavaScript, HTML, CSS</h3>
+    </div>
+  )
+}
+
+export default TechSkills
--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,27 @@ import IntroSection from '../components/Intro.jsx'
 import ProjectsSection from '../components/Projects.jsx'
 import EducationSection from '../components/Education.jsx'
 import ExperienceSection from '../components/Experience.jsx'
+import TechSkills from '../components/TechSkills.jsx'
+
+window.addEventListener('scroll', () => {
+  document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="sun"> 
+        <div className="sun2"></div>
+      </div>
+      <div className="parentDiv">
+        <img className="moon" src="./src/assets/Moon.png" alt="" />
+      </div>
 
-      <h1>Sam Adams' Resume</h1>
+      {/* <h1>Sam Adams' Resume</h1> */}
       <IntroSection className="section"/>
+      <TechSkills />
       <br />
       <br />
       <ProjectsSection />
