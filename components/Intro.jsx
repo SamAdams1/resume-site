26a0e9f1e8a843920c696e61b52fd5f6814132f6
Sam Adams <119465691+SamAdams1@users.noreply.github.com>
2024-09-29 11:13:38 -0400

    update project sections

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,14 +1,27 @@
-import { SocialIcon } from 'react-social-icons';
-function IntroSection(){
-    return (
-        <div className='introSect'>
-            {/* <h3>Phone: 603-552-8937</h3>
+import { SocialIcon } from "react-social-icons";
+function IntroSection() {
+  return (
+    <div className="introSect">
+      {/* <h3>Phone: 603-552-8937</h3>
             <h3>Email: sammyadams04@gmail.com</h3> */}
-            <h1 id='myName'>Sam Adams</h1>
-            <h3>A Software Engineer passionate about app and web development, committed to delivering innovative solutions. I am recognized for my resourcefulness and unwavering curiosity when tackling complex challenges. Eager to contribute my skills and drive to a dynamic team.</h3>
-            <SocialIcon className='socialIcons' url='https://www.linkedin.com/in/sam-adams-87a158269/' target='_blank'/>
-            <SocialIcon className='socialIcons' url='https://github.com/SamAdams1?tab=repositories' target='_blank'/>
-        </div>
-    )
+      <h1 id="myName">Sam Adams</h1>
+      <h3>
+        A Software Engineer passionate about web development, committed to
+        delivering innovative solutions. I am recognized for my resourcefulness
+        and unwavering curiosity when tackling complex challenges. Eager to
+        contribute my skills and drive to a dynamic team.
+      </h3>
+      <SocialIcon
+        className="socialIcons"
+        url="https://www.linkedin.com/in/sam-adams-87a158269/"
+        target="_blank"
+      />
+      <SocialIcon
+        className="socialIcons"
+        url="https://github.com/SamAdams1?tab=repositories"
+        target="_blank"
+      />
+    </div>
+  );
 }
-export default IntroSection
+export default IntroSection;
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,68 +1,86 @@
-import { SocialIcon } from 'react-social-icons';
+import { SocialIcon } from "react-social-icons";
 
 const projectsDetailsArr = [
-{
-title: "Cosmic Campaign",
-desc: "A fast-paced top-down space shooter rougelike game. Learned how to plan a large project, transfer skills, learn new technology quickly, brainstorm ideas, navigate documentation, and fix bugs.",
-tech: "GDScript, Godot Engine, LibreSprite, AudioMass, GitHub",
-repoLink: "https://github.com/SamAdams1/Cosmic-Campaign",
-demoLink: "https://sams-studio.itch.io/cosmic-campaign",
-},
-{
-title: "Historical Battle Map",
-desc: "A map that displays important historical battles on a world map. Learned how to use APIs, use web sockets, connect to a primary database, and create user authentication.",
-tech: "Leaflet Api, Wikipedia Api, JavaScript, HTML, CSS",
-repoLink: "https://github.com/SamAdams1/battles-map",
-demoLink: "https://samadams1.github.io/battles-map/",
-},
-{
-  title: "Sahara",
-  desc: `Amazon clone to learn about React, MongoDB, 
- ExpressJS, user registration/login, and user data 
- security. Developed a deeper understanding of 
- web design principles and good practices.`,
-  tech: "MongoDB, JavaScript, DummyJson Api, ExpressJS, Vite, React",
-  
-  repoLink: "https://github.com/SamAdams1/fakeazon",
-},
-// {
-// title: "AudioBookify",
-// desc: `Upload a PDF or .txt file and it will be automatically read and turned into an audiobook!
-// This audiobook can then be downloaded to be listened to later offline.
-// Change the speed of you narrator. Easily fastforward or rewind your audiobook.`,
-// tech: ["MongoDB, Ayfie Api, ExpressJS, Google-Text-To-Speech"],
+  {
+    title: "Historical Battle Wiki",
+    desc: "A map that displays important historical battles on a world map, allows history nerds to talk to eachother, and provides a wiki to add new battles to the database. A passion project that combines my love of history and coding.",
+    tech: "JavaScript, React, Expressjs, Nodejs, MongoDB, Python, TailwindCSS, JWT, Bcrypt, Google Cloud Run, Vercel, Websockets",
+    repoLink: "https://github.com/SamAdams1/battles-map",
+    demoLink: "https://samadams1.github.io/battles-map/",
+  },
+  {
+    title: "Austrian Castle Locator",
+    desc: `A website that displays all the castles in Austria. Similar to the Battle Wiki but uses a totally different tech stack as I was curious about the technologies.`,
+    tech: "TypeScript, Angular, Java, Spring Boot, PostgreSQL, Vercel, Docker, AWS",
 
-// repoLink: "https://github.com/AndrewJhD/Course2Group16-sProject",
-// },
-]
+    repoLink: "http://github.com/SamAdams1/castle-finder",
+    demoLink: "https://castle-finder.vercel.app/",
+  },
+  {
+    title: "Cosmic Campaign",
+    desc: "A fast-paced top-down space shooter rougelike game. Learned how to plan a large project, learn new technology quickly, brainstorm ideas, navigate documentation, and fix bugs.",
+    tech: "GDScript, Godot Engine, Git, GitHub",
+    repoLink: "https://github.com/SamAdams1/Cosmic-Campaign",
+    demoLink: "https://sams-studio.itch.io/cosmic-campaign",
+  },
+
+  // {
+  // title: "AudioBookify",
+  // desc: `Upload a PDF or .txt file and it will be automatically read and turned into an audiobook!
+  // This audiobook can then be downloaded to be listened to later offline.
+  // Change the speed of you narrator. Easily fastforward or rewind your audiobook.`,
+  // tech: ["MongoDB, Ayfie Api, ExpressJS, Google-Text-To-Speech"],
+
+  // repoLink: "https://github.com/AndrewJhD/Course2Group16-sProject",
+  // },
+];
 
 function ProjectsSection() {
-return(
-  <>
-    {/* <button>DarkMode</button> */}
-    <h1 id='projects' className='projectsTitle'>Projects</h1>
-    <div className='projectsSect'>
-      {projectsDetailsArr.map((projectDict, arrIndex) => (
-        <div className='singleProject'>
-          <h2 key={projectDict.title} className='projectTitle'>{projectDict.title}</h2>
-          
-          <h3 className='projectDesc'>{projectDict.desc}</h3>
+  return (
+    <>
+      {/* <button>DarkMode</button> */}
+      <h1 id="projects" className="projectsTitle">
+        Projects
+      </h1>
+      <div className="projectsSect">
+        {projectsDetailsArr.map((projectDict, arrIndex) => (
+          <div className="singleProject">
+            <h2 key={projectDict.title} className="projectTitle">
+              {projectDict.title}
+            </h2>
 
-          {projectDict.tech.split(", ").map((techStr , techIndex) => (
-            <h4 className='projectTech' key={`${techStr}${techIndex}`}>{techStr}</h4>
-          ))}
+            <h3 className="projectDesc">{projectDict.desc}</h3>
 
-          <div className="projectLinks">
-            <a href={projectDict.repoLink} target='_blank' className='codeLink'>View Code</a>
-            {projectDict.demoLink && <a href={projectDict.demoLink} target='_blank' className='siteLink'>Visit Site</a>}
-            {/* Will show second icon if there is a demo link */}
+            {projectDict.tech.split(", ").map((techStr, techIndex) => (
+              <h4 className="projectTech" key={`${techStr}${techIndex}`}>
+                {techStr}
+              </h4>
+            ))}
+
+            <div className="projectLinks">
+              <a
+                href={projectDict.repoLink}
+                target="_blank"
+                className="codeLink"
+              >
+                View Code
+              </a>
+              {projectDict.demoLink && (
+                <a
+                  href={projectDict.demoLink}
+                  target="_blank"
+                  className="siteLink"
+                >
+                  Visit Site
+                </a>
+              )}
+              {/* Will show second icon if there is a demo link */}
+            </div>
           </div>
+        ))}
+      </div>
+    </>
+  );
+}
 
-        </div>
-      ))}
-    </div>
-  </>
-)
-};
-
-export default ProjectsSection
+export default ProjectsSection;
